4fa61c2ba7c9b161500415acd1009bedb6e03b7b
Ahamed Imran <immy123@gmail.com>
2017-09-10 15:05:11 +0530

    add dynamic form module

---
+++ src/app/dynamic-form/dynamic-form.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { DynamicFormComponent } from './containers/dynamic-form/dynamic-form.component';
+import { FormInputComponent } from './components/form-input/form-input.component';
+import { FormButtonComponent } from './components/form-button/form-button.component';
+import { FormSelectComponent } from './components/form-select/form-select.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    DynamicFormComponent,
+    FormInputComponent,
+    FormButtonComponent,
+    FormSelectComponent
+  ],
+  exports: [DynamicFormComponent]
+})
+export class DynamicFormModule { }
