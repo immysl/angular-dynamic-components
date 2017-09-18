fa02735af0d5b3eaa40be6f74b6205a4e9d8edcd
Ahamed Imran <immy123@gmail.com>
2017-09-18 17:33:49 +0530

    make dynamic form handle the JSON schema (configList) instead of form template

--- src/app/dynamic-form/containers/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/containers/dynamic-form/dynamic-form.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
+import { DataService } from '../../../shared/data.service';
+import { CommonStore } from '../../../shared/common-store';
+
 import { DynamicFieldConfig } from '../../models/dynamic-field-config';
+import { State } from '../../../shared/models/state';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -9,15 +13,44 @@ import { DynamicFieldConfig } from '../../models/dynamic-field-config';
   styleUrls: ['./dynamic-form.component.scss']
 })
 export class DynamicFormComponent implements OnInit {
-  @Input() configList: DynamicFieldConfig[] = [];
   @Output() submitted: EventEmitter<any> = new EventEmitter<any>();
 
   form: FormGroup;
+  configList: DynamicFieldConfig[];
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private dataService: DataService,
+    private commonStore: CommonStore
+  ) { }
 
   ngOnInit(): void {
-    this.form = this.createGroup();
+    this.getConfigList();
+    this.subscribeConfigListChanges();
+  }
+
+  getConfigList() {
+    this.dataService
+      .getSimpleForm()
+      .subscribe((data: DynamicFieldConfig[]) => {
+        const currentState: State = this.commonStore.getState();
+
+        // update formConfigList in an immutable way
+        this.commonStore.setState(
+          Object.assign({}, currentState, { formConfigList: data })
+        );
+      });
+  }
+
+  subscribeConfigListChanges() {
+    this.commonStore
+      .changes
+      .subscribe((state: State) => {
+        if (state.formConfigList) {
+          this.configList = state.formConfigList;
+          this.form = this.createGroup();
+        }
+      });
   }
 
   createGroup(): FormGroup {
--- src/app/form-template/form-template/form-template.component.ts
+++ src/app/form-template/form-template/form-template.component.ts
@@ -1,40 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { DataService } from '../../shared/data.service';
-import { CommonStore } from '../../shared/common-store';
-
-import { DynamicFieldConfig } from '../../dynamic-form/models/dynamic-field-config';
-import { State } from '../../shared/models/state';
-
 @Component({
   selector: 'app-form-template',
   templateUrl: './form-template.component.html',
   styleUrls: ['./form-template.component.scss']
 })
 export class FormTemplateComponent implements OnInit {
-  configList: DynamicFieldConfig[];
+  constructor() { }
 
-  constructor(
-    private dataService: DataService,
-    private commonStore: CommonStore
-  ) { }
-
-  ngOnInit(): void {
-    this.dataService
-      .getSimpleForm()
-      .subscribe((data: DynamicFieldConfig[]) => {
-        const currentState: State = this.commonStore.getState();
-
-        // update formConfigList in an immutable way
-        this.commonStore.setState(
-          Object.assign({}, currentState, { formConfigList: data })
-        );
-      });
-
-    this.commonStore
-        .changes
-        .subscribe((state: State) => this.configList = state.formConfigList);
-  }
+  ngOnInit(): void { }
 
   processForm(formValues): void {
     console.log(formValues);
