d5e47589548b6de5f4938313ce2059f5577af926
Ahamed Imran <immy123@gmail.com>
2017-09-19 17:44:30 +0530

    remove unused modules from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,21 @@
+import { FormInventoryComponent } from './form-inventory/form-inventory.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-import { LabelModule } from '@progress/kendo-angular-label';
-import { InputsModule } from '@progress/kendo-angular-inputs';
-import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 import { FormTemplateModule } from './form-template/form-template.module';
 
 import { AppComponent } from './app.component';
 import { DataService } from './shared/data.service';
 import { CommonStore } from './shared/common-store';
-import { FieldFormComponent } from './field-form/field-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FieldFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    DropDownsModule,
-    LabelModule,
-    InputsModule,
-    ButtonsModule,
     FormTemplateModule
   ],
   providers: [
